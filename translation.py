e63ec32bd7dab040667546df13ab56f2b7771a7c
RINCEMON REJI <70466997+RincemonReji@users.noreply.github.com>
2021-01-08 22:21:33 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
     START_MSG = """<b>Hello,
 
-I am a Simple Bot Forward The Files You Send to Me to your Channel</b>""" 
+I am a Simple Bot Forward The Files You Send to Me to your Channel, made by @filmsandtrends_owner</b>""" 
     HELP = """<code>Send me Any Medias to Forward to the Channel</code>"""  
